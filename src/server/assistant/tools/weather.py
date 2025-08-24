e67a2f4aa3d3d11c6acd835bbb3a45b50d849212
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-08-24 15:12:03 -0700

    Use triple quoted strings for tool descriptions

--- src/server/assistant/tools/weather.py
+++ src/server/assistant/tools/weather.py
@@ -1,21 +1,20 @@
 import clients.weather as weather_client
-from utils.decorators import add_docstring
 from logging import Logger
 from typing import Callable, Awaitable
 import httpx
 
 
 def forecast_tool(
-    weather_httpx_client: httpx.AsyncClient, 
-    geocode: Callable[[str], Awaitable[tuple[float, float]]], 
-    logger: Logger
+    weather_httpx_client: httpx.AsyncClient,
+    geocode: Callable[[str], Awaitable[tuple[float, float]]],
+    logger: Logger,
 ):
-    description = "Returns a weather forecast formatted as JSON."
-
-    @add_docstring(description)
     async def forecast(location: str) -> str:
+        """Returns a weather forecast formatted as JSON."""
         lat, lon = await geocode(location)
-        forecast = await weather_client.twelve_hour_forecast(weather_httpx_client, logger, lat, lon)
+        forecast = await weather_client.twelve_hour_forecast(
+            weather_httpx_client, logger, lat, lon
+        )
         return forecast.model_dump_json()
 
     return forecast
--- src/server/utils/decorators.py
+++
@@ -1,6 +0,0 @@
-def add_docstring(doc):
-    def decorator(func):
-        func.__doc__ = doc
-        return func
-
-    return decorator
